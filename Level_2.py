e4a7223dfebd2919f8197c9999405fd948e9efd0
bomi-yun <38641258+bomi-yun@users.noreply.github.com>
2018-04-27 18:31:19 +0900

    level2
    level2 +

--- Level_2.py
+++ Level_2.py
@@ -9,12 +9,18 @@ class B_Level_2:
         #    print(o["no"] +" : "+ o["title"])
     
     def excute(self, no):
+        print(str(no) + u"실행")
         for o in self.list:
             if no == o["no"]:
                 o["method"]()
 
     def initList(self):
         self.addList('2741', self.L2741)
+        self.addList('2742', self.L2742)
+        self.addList('2739', self.L2739)
+        self.addList('2438', self.L2438)
+        self.addList('2439', self.L2439)
+        self.addList('2440', self.L2440)
         
     def addList(self, obj, method):
         self.list.append({
@@ -26,3 +32,29 @@ class B_Level_2:
         input = int(raw_input())
         for i in range(0, input):
             print(i+1)
+
+    
+    def L2742(self):
+        input = int(raw_input())
+        for i in reversed(range(0, input)):
+            print(i+1)
+
+    def L2739(self):
+        input = int(raw_input())
+        for i in range(1, 10):
+            print '{0} * {1} = {2}'.format(str(input), str(i), str(input * i))
+
+    def L2438(self):
+        input = int(raw_input())
+        for i in range(1, input+1):
+            print '*'*i
+            
+    def L2439(self):
+        input = int(raw_input())
+        for i in range(1, input+1):
+            print '{0}{1}'.format(' '*(input-i), '*'*i)
+            
+    def L2440(self):
+        input = int(raw_input())
+        for i in reversed(range(1, input+1)):
+            print '*'*i
